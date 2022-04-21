8197a4f2df960fe7c6b605880cbdce89e80f0839
Khang <khang.nguyen.trd@gmail.com>
2022-04-21 10:44:14 -0500

    Initial Commit

---
+++ WeatherClone/WeatherCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherCloneApp.swift
+//  WeatherClone
+//
+//  Created by Nguyen Tran Duy Khang on 4/21/22.
+//
+
+import SwiftUI
+
+@main
+struct WeatherCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
